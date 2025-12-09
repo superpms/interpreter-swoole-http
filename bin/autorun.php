ff8a13f44df4e880f864fef763f6300a1cf18421
Guest <2434981942@qq.com>
2025-12-09 11:29:57 +0800

    规范Terminal类型App挂载

--- bin/autorun.php
+++ bin/autorun.php
@@ -5,7 +5,6 @@ use pms\hook\TerminalCommandHook;
 
 if(class_exists('pms\hook\TerminalCommandHook')){
     TerminalCommandHook::mount(
-        'swoole-http-server',
         program\swooleHttp\SwooleHttpCommand::class
     );
 }
